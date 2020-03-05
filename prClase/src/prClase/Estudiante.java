7bc6a5f758b4d18af2ad45cbd6eb5d1be1263b58
Alumno <Alumno@DESKTOP-289CM2L>
2020-03-05 13:32:07 +0100

    Prueba

---
+++ prClase/src/prClase/Estudiante.java
@@ -0,0 +1,92 @@
+package prClase;
+
+public class Estudiante {
+	
+	// Variables de instancia (guardan información del alumno)
+	private String nombre;
+	private byte edad;
+	private String dni;
+	
+	/**
+	 * Constructor
+	 * @param nombre
+	 * @param edad
+	 * @param dni
+	 */
+	
+	public Estudiante(String nombre, byte edad, String dni) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.dni = dni;
+		
+		
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public byte getEdad() {
+		return edad;
+	}
+
+	public void setEdad(byte edad) {
+		this.edad = edad;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	@Override
+	public String toString() {
+		return this.nombre + " tiene " +this.edad+ " y su DNI es " +this.dni;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + edad;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estudiante other = (Estudiante) obj;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (edad != other.edad)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
---
+++ prClase/src/prClase/TestAlumno.java
@@ -0,0 +1,16 @@
+package prClase;
+
+public class TestAlumno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Estudiante alumno1 = new Estudiante("Pepe",(byte)21,"11121212");
+		Estudiante alumno2 = new Estudiante("María",(byte)20,"343434212");
+
+		System.out.println(alumno1.toString());
+		System.out.println(alumno2.toString());
+		
+	}
+
+}
